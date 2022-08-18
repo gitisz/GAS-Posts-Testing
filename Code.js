3545d7007123fbcadafb5ba6c34b51fddf527711
james green <jamesgreen.311@gmail.com>
2022-08-18 09:55:43 -0400

    Upload Image 0.1

--- Code.js
+++ Code.js
@@ -1,51 +1,55 @@
 const sheetId = '1n-BYfns_ki5UlI51PZL_5p4WdCF2fVOgHA4TbGXPfOo'
 
-function doPost(request) {
-  const body = request.postData.contents;
-  const bodyJSON = JSON.parse(body)
-  const ss = SpreadsheetApp.openById(sheetId)
-  const ws = ss.getSheetByName("Sheet1")
-  const fileid = ""
-  const member = "Yes"
-  const availability = ""
-  const hidden = ""
-  const fullname = bodyJSON.firstname + " " + bodyJSON.lastname
-  const row = [
-    bodyJSON.eventid,
-    bodyJSON.eventtitle,
-    bodyJSON.firstname,
-    bodyJSON.lastname,
-    bodyJSON.email,
-    bodyJSON.phone,
-    bodyJSON.worktitle,
-    bodyJSON.medium,
-    bodyJSON.width,
-    bodyJSON.height,
-    bodyJSON.price,
-    bodyJSON.filename,
-    fileid,
-    member,
-    availability,
-    hidden,
-    fullname,
-    bodyJSON.timestamp
-  ]
-  ws.appendRow(row)
-  const response = {status: 200, data: row}
-  return JSON.stringify(response)
-}
-  //const path = request.parameter['q']
-  //const id = e.parameter['id']
-/*   const result = route(path, request)
-
-  const response = JSON.stringify(result)
+// this function executes from html-form
+function doPost(e) {
+  var data = Utilities.base64Decode(e.parameters.data);
+  var blob = Utilities.newBlob(data, e.parameters.mimetype, e.parameters.filename);
+  // save image to selected folder
+  var folder = DriveApp.getFolderById('1JL85kWwmAPWqyI-0c735N06a4EDpPGvZ');                      // your folder id
+  var file = folder.createFile(blob);
   
-  return ContentService
-      .createTextOutput(response)
-      .setMimeType(ContentService.MimeType.JSON) 
-  console.log(request)*/
+  // put the image with the help of a formula in a cell in Google Sheets
+  // https://webapps.stackexchange.com/questions/86081/insert-image-from-google-drive-into-google-sheets
+  var ss = SpreadsheetApp.openById(sheetId);              // your spreadsheet id
+  var r = ss.getSheetByName('Sheet1').getRange('S2');                                        // your sheet name and cell-address
+  var id = file.getId();
+  r.setFormula('=IMAGE("https://docs.google.com/uc?export=view&id=' + id + '")');
+  
 
+  // here I want browser window to close 
+  PropertiesService.getScriptProperties().setProperty('my_unique_key', 'Done')  
+  var output = HtmlService.createHtmlOutput("<b>Done!</b>");
+  output.setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+  return output;
+}
+function doGet(e) {
+}
 
+// this function openes dialog window
+function openImageForm()
+{  
+  var html = HtmlService.createHtmlOutputFromFile('UploadForm');
+  SpreadsheetApp.getUi() // Or DocumentApp or SlidesApp or FormApp.
+      .showModalDialog(html, 'Select image'); 
+  
+}
 
-
-//ROUTES.path("add", addSubmission)
+// this function executes from HTML-form and returnes signal to close the window
+// after successful loading of the file to Drive
+function clientSideFunctionToProcessReturnedData()
+{
+  var key = 'my_unique_key';
+  var props = PropertiesService.getScriptProperties()
+  
+  while (true)
+  {
+    var result = props.getProperty(key);
+    if (result === 'Done')
+    {
+      props.deleteProperty(key);
+      return true;
+    } 
+    // check each second
+    Utilities.sleep(1000);
+  }
+}
